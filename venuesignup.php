8d7b0da5ca6e74f1d484d618c2e481ad09bb6525
Anjani Anusuri <38348586+AnjaniAnusuri@users.noreply.github.com>
2019-11-24 18:24:04 +1000

    Customer

--- venuesignup.php
+++ venuesignup.php
@@ -23,15 +23,18 @@
                         echo "<p class='text-danger'>You already sent a request to us, we'll get in touch with you soon</p>";
                     }
                 }
+                else if(isset($_GET['signup'])){
+                  if($_GET['signup'] === 'success'){
+                  echo "<p class='text-success'>We'll get in touch with you soon</p>";
+                }
+                }
             ?>
-            <label for="name">Venue Name</label>
-            <input class = "form-control" type="text" id="name" name="name" value="<?php echo $name;?>" placeholder="Enter your Venue Name" />
+            <label for="name">Name</label>
+            <input class = "form-control" type="text" id="name" name="name" value="<?php echo $name;?>" placeholder="Enter your first name" />
             <small id="nMessage" class="form-text text-muted"></small>
             <label for="email">E-Mail</label>
             <input class = "form-control" type="text" id="email" name="email" value="<?php echo $email;?>" placeholder="Enter your email address" />
             <small id="eMessage" class="form-text text-muted"></small>
-            <label for="password">Password</label>
-            <input class = "form-control" type="password" id="password" name="password" value="" placeholder="Enter your Password" />
             <small id="passwordMessage" class="form-text text-muted"></small>
             <input class="btn btn-primary" type="submit" name="submit" id="submit"/>
         </form>
