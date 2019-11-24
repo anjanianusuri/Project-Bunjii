808435faedf6fa86794175f712efa82cd263e7ff
Bhargavbandi <38348586+Bhargavbandi@users.noreply.github.com>
2019-11-24 17:46:21 +1000

    Update database.php

--- database.php
+++ database.php
@@ -1,16 +1,8 @@
 <?php
 
-
-
- //   $conn = mysqli_connect('localhost', 'root', '','project');
-
-   $conn = mysqli_connect('localhost', 'ictatjcu_bunjii', '123zxc','ictatjcu_bunjii');
-
-
+    $conn = mysqli_connect('localhost', 'root', '','project');
+//    $conn = mysqli_connect('localhost', 'ictatjcu_bunjii', '123zxc','ictatjcu_bunjii');
 
     if(!$conn) {
-
         die("Connection failed: ".mysqli_connect_error());
-
     }
-
